8560bd877836c698da05524e075c48eab4325018
David Roche <rochedjd@gmail.com>
2023-06-14 21:35:19 +0100

    Added Upcoming movies page.

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,9 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader';
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage"; // lab 4 exercise
+
+
 
 const App = () => {
   return (
@@ -14,6 +17,7 @@ const App = () => {
       <Routes>
         <Route path="/reviews/:id" element={<MovieReviewPage />} />
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
         <Route path="/movies/:id/:isFav" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
