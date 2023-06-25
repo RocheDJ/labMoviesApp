a2fe49e8266c74b6b4c23264ace181a5f17c6046
David Roche <rochedjd@gmail.com>
2023-06-25 10:01:44 +0100

    Fix upcoming movies page.

--- src/pages/upcomingMoviesPage.jsx
+++ src/pages/upcomingMoviesPage.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { getUpCommingMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage'
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
 
 const UpcomingMoviesPage = (props) => {
   const [movies, setMovies] = useState([]);
-  const favourites = movies.filter(m => m.favourite)
-  localStorage.setItem('favourites', JSON.stringify(favourites))
+  
+  // const favourites = movies.filter(m => m.favourite)
+  // localStorage.setItem('favourites', JSON.stringify(favourites))
 
-  const addToFavourites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favourite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
+  // const addToFavourites = (movieId) => {
+  //   const updatedMovies = movies.map((m) =>
+  //     m.id === movieId ? { ...m, favourite: true } : m
+  //   );
+  //   setMovies(updatedMovies);
+  // };
 
   useEffect(() => {
     getUpCommingMovies().then(movies => {
@@ -25,7 +27,9 @@ const UpcomingMoviesPage = (props) => {
     <PageTemplate
       title='Upcomming Movies'
       movies={movies}
-      selectFavourite={addToFavourites}
+      action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
     />
   );
 };
